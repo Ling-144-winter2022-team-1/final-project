6fb2213f72b73d60c848ab32a5e965c052a2795e
IsaPolena <97200386+IsaPolena@users.noreply.github.com>
2022-03-05 23:22:26 +0000

    Removed male and '?' characters

--- Data/CorpusFiles/Filters.py
+++ Data/CorpusFiles/Filters.py
@@ -1,11 +1,26 @@
 import pandas as pd
 
-#  Lines containing characters with gender set to '?':
+#       Removing lines containing characters with gender set to                                             '?'
 #  read_csv
-Gender_data = pd.read_csv('characters.csv', index_col ='Gender')
+Gender_data = pd.read_csv("characters.csv", index_col ="Gender")
 
 # retrieving rows by loc method
-unkown_gender = Gender_data.loc['?']
+unkown_gender = Gender_data.loc[['m','M','f','F']]
 
 print("\n Unknown Gender Data\n")
 print(unkown_gender)
+unkown_gender.to_csv('all_characters.csv')
+
+#       Removing lines containing characters with gender set to                                         'M' and 'm'
+#  read_csv
+Gender_data = pd.read_csv("characters.csv", index_col ="Gender")
+
+# retrieving rows by loc method
+unkown_gender = Gender_data.loc[['f','F']]
+
+print("\n Unknown Gender Data\n")
+print(unkown_gender)
+unkown_gender.to_csv('female_characters.csv')
+
+for genders in female_characters.csv:
+  

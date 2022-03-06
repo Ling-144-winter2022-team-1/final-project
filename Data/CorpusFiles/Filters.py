51db4702ebde967fe23a77d135bd58b158835346
IsaPolena <97200386+IsaPolena@users.noreply.github.com>
2022-03-06 00:42:47 +0000

    created list of female IDs and saved in txt file

--- Data/CorpusFiles/Filters.py
+++ Data/CorpusFiles/Filters.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 
 #       Removing lines containing characters with gender set to                                             '?'
 #  read_csv
@@ -16,11 +17,31 @@ unkown_gender.to_csv('all_characters.csv')
 Gender_data = pd.read_csv("characters.csv", index_col ="Gender")
 
 # retrieving rows by loc method
-unkown_gender = Gender_data.loc[['f','F']]
+female_gender = Gender_data.loc[['f','F']]
 
-print("\n Unknown Gender Data\n")
-print(unkown_gender)
-unkown_gender.to_csv('female_characters.csv')
+print("\n Female Gender Data\n")
+print(female_gender)
+female_gender.to_csv('female_characters.csv')
 
-for genders in female_characters.csv:
-  
+#         Isolating female character IDs
+
+# reading CSV file
+female_characters_data = pd.read_csv("female_characters.csv")
+
+print("\n Female characters data \n")
+print(female_characters_data)
+
+# converting column data to list
+ChID = female_characters_data['CharacterID'].tolist()
+
+#print("\n Female Character IDs\n")
+#print('CharacterID:', ChID)
+
+textfile = open("femaleID_list.txt", "w")
+for element in ChID:
+    textfile.write(element + "\n")
+textfile.close()
+
+#print("\n femaleID_list. txt file\n")
+
+#          Using character IDs to filter
